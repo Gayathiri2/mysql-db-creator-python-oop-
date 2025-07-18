12b5ba508a9de74d39c3ed0e2a27ca402530898a
Gayathiri Devi <169134423+Gayathiri2@users.noreply.github.com>
2025-07-18 12:44:19 +0530

    Update MySQL_database_project.sql

--- MySQL_database_project.sql
+++ MySQL_database_project.sql
@@ -1,19 +1,8 @@
 show databases;
-use gayu;
-create table data (
-    id int primary key auto_increment,
-	name varchar(30),
-    age int,
-    address varchar(100),
-    contact varchar(100),
-    mail varchar(50)
-    );
-    ALTER TABLE data  MODIFY contact VARCHAR(15);
-    drop table data;
-    select * from data;
+use enter_your_database_name;
     create table data (
     id int primary key auto_increment,
-	name varchar(30),
+    name varchar(30),
     age int,
     address varchar(100),
     contact varchar(15),
